1585eba3c845a56207a7bca613e1856a1131a4e6
Jason Wijaya <jasonwijaya2@gmail.com>
2025-04-10 09:08:50 +0700

    calculate square

--- jest.test.js
+++ jest.test.js
@@ -1,16 +1,31 @@
-const add = require('./utils')
+const calculateSquare = require('./utils')
 
-describe('menjumlahkan 1 + 2 = 3', () => {
+describe('calculateSquare', () => {
     it("should return 5 when x = 3 and y = 2", () => {
         //Given
         const x = 3
         const y = 2
 
         //When
-        const result = add(x, y)
+        const result = calculateSquare(x, y)
 
         //Then
         const expectedResult = 5
+
+        expect(result).toBe(expectedResult)
+    })
+
+    it("should return 1000 when x, y, z are all 10", () => {
+        //Given
+        const x = 10
+        const y = 10
+        const z = 10
+
+        //When
+        const result = calculateSquare(x, y, z)
+
+        //Then
+        const expectedResult = 1000
 
         expect(result).toBe(expectedResult)
     })
--- utils.js
+++ utils.js
@@ -1,4 +1,8 @@
-function add(a, b) {
-    return a + b;
+function calculateSquare(x, y, z) {
+    if (z) {
+        return x * y * z
+    }
+
+    return x + y;
 }
-module.exports = add;
+module.exports = calculateSquare;
